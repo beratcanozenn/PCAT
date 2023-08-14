04699f149de47beb293b66931d356b6578ade60a
Beratttt <beratozen2510@gmail.com>
2023-08-15 01:47:01 +0300

    Setting HTML pages to ejs

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 import express from "express";
+import path from "path";
+import ejs from "ejs";
+
+const app = express();
+
+// Template Engine
+app.set("view engine", "ejs");
+
+// Middlewares
+app.use(express.static("public"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`);
+});

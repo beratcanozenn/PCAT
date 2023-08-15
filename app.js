c64abfcc10326d08a22c2583e574f8ce3b83f617
Beratttt <beratozen2510@gmail.com>
2023-08-15 14:27:08 +0300

    Mongoose Added

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-import express from "express";
-import path from "path";
-import ejs from "ejs";
+const express = require("express");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const Photo = require("./models/Photo");
 
 const app = express();
+// DB Connect
+mongoose.connect("mongodb://localhost/pcat-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Template Engine
 app.set("view engine", "ejs");
 
 // Middlewares
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({});
+
+  res.render("index", { photos });
 });
 
 app.get("/about", (req, res) => {
@@ -21,6 +31,22 @@ app.get("/about", (req, res) => {
 
 app.get("/add", (req, res) => {
   res.render("add");
+});
+
+app.post("/photos", async (req, res) => {
+  const { title, description, image } = req.body;
+  const photo = new Photo({
+    title,
+    description,
+    image,
+  });
+
+  await photo
+    .save()
+    .then((photo) => console.log("Veri başarıyla eklendi :"))
+    .catch((err) => console.log("Veri eklerken Hata oluştu : " + err));
+
+  res.redirect("/");
 });
 
 const port = 3000;
